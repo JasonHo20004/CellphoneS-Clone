8a8fdd2588e2fd0d291b851dcc6341f58717ab04
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-09-23 15:39:25 +0700

    Add initial project files and configurations

---
+++ backend/src/main/java/com/example/cellphonesclone/CellphonescloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.cellphonesclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellphonescloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellphonescloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/cellphonesclone/CellphonescloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cellphonesclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellphonescloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
