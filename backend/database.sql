bef6772fd438f795c1c4512b479f69e4fb4f9deb
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-10-13 17:39:52 +0700

    update database

--- backend/database.sql
+++ backend/database.sql
@@ -2,43 +2,42 @@ CREATE DATABASE CellphoneSClone
 USE CellphoneSClone
 
 -- Bang Users
-CREATE TABLE Users(
+CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT,
     fullname VARCHAR(100) DEFAULT '',
     phonenumber VARCHAR(10) NOT NULL,
     user_address VARCHAR(200) DEFAULT '',
-    user_password VARCHAR(100) NOT NULL DEFAULT '', --mật khẩu đã được mã hóa
+    user_password VARCHAR(100) NOT NULL DEFAULT '', -- mật khẩu đã được mã hóa
     created_at DATETIME,
     updated_at DATETIME,
     is_active TINYINT(1) DEFAULT 1,
     date_of_birth DATETIME,
     facebook_account_id INT DEFAULT 1,
     google_account_id INT DEFAULT 1,
-    PRIMARY KEY (id)
+    role_id INT, -- Thêm cột role_id
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) -- Thêm ràng buộc khóa ngoại cho role_id
 );
-ALTER TABLE users ADD COLUMN role_id INT;
-ALTER TABLE users ADD FOREIGN KEY(role_id) REFERENCES roles(id);
 
 --Bang Roles
-CREATE TABLE Roles(
+CREATE TABLE roles(
     id INT NOT NULL,
     name VARCHAR(20) NOT NULL
     PRIMARY KEY(id),
 );
 
 --Bang Tokens
-CREATE TABLE Tokens (
+CREATE TABLE tokens (
     token_id INT NOT NULL AUTO_INCREMENT,
-    token_value VARCHAR(255) NOT NULL,
+    token_value VARCHAR(255) NOT NULL UNIQUE, -- Thêm ràng buộc UNIQUE trực tiếp vào cột token_value
     token_type VARCHAR(50) NOT NULL,
     expiration_date DATETIME,
     revoked TINYINT(1) NOT NULL,
     expired TINYINT(1) NOT NULL,
     user_id INT,
     PRIMARY KEY (token_id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-ALTER TABLE Tokens ADD CONSTRAINT UNIQUE (token_value);
 
 --Ho tro dang nhap bang Facebook va Google
 CREATE TABLE social_account(
@@ -58,40 +57,51 @@ CREATE TABLE brands(
 );
 
 --Bang chua san pham
-CREATE TABLE products(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(350),
-    price FLOAT NOT NULL CHECK(price >= 0),
-    thumbnail VARCHAR(350) DEFAULT '',
-    DESCRIPTION LONGTEXT DEFAULT '',
+CREATE TABLE products (
+    product_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(300),
+    price FLOAT NOT NULL CHECK (price >= 0),
+    thumbnail VARCHAR(350),
+    description LONGTEXT DEFAULT '',
     created_at DATETIME,
     updated_at DATETIME,
     brands_id INT,
+    in_stock INT,
+    operating_system VARCHAR(255),
+    screen_size DECIMAL(5,2),
+    battery_capacity INT,
+    ram INT,
+    rom INT,
+    front_camera VARCHAR(350),
+    main_camera VARCHAR(350),
+    color VARCHAR(255),
+    release_date DATETIME,
+    PRIMARY KEY (product_id),
     FOREIGN KEY (brands_id) REFERENCES brands(id)
 );
 
 --Bang orders
-CREATE TABLE orders(
+CREATE TABLE orders (
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
-    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     fullname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     phonenumber VARCHAR(20) NOT NULL,
-    address VARCHAR(200) NOT NUll,
+    address VARCHAR(200) NOT NULL,
     note VARCHAR(100) DEFAULT '',
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20),
-    total_money FLOAT CHECK(total_money >= 0)
+    status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled'), -- Modified status with ENUM
+    total_money FLOAT CHECK (total_money >= 0),
+    shipping_method VARCHAR(100), -- Added shipping_method
+    shipping_address VARCHAR(200), -- Added shipping_address
+    shipping_date DATE, -- Added shipping_date
+    tracking_number VARCHAR(100), -- Added tracking_number
+    payment_method VARCHAR(100) -- Added payment_method
 );
-ALTER TABLE orders ADD COLUMN shipping_method VARCHAR(100); 
-ALTER TABLE orders ADD COLUMN shipping_address VARCHAR (200); 
-ALTER TABLE orders ADD COLUMN shipping_date DATE;
-ALTER TABLE orders ADD COLUMN tracking_number VARCHAR(100);
-ALTER TABLE orders ADD COLUMN payment_method VARCHAR(100);
-ALTER TABLE orders MODIFY COLUMN status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled')
 
 
+-- Trong bảng orders lưu fullname, email, phonenumber khác vì người dùng có thể nhập khác so với bảng users
 
 CREATE TABLE order_details(
     id INT PRIMARY KEY AUTO_INCREMENT,
