4dd5e51c0628930405b0ddabc77fb220ee7b5b8e
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-11-15 13:04:57 +0700

    Add JWT token utility and update login functionality

--- backend/src/main/java/com/example/cellphonesclone/services/IUserService.java
+++ backend/src/main/java/com/example/cellphonesclone/services/IUserService.java
@@ -6,5 +6,5 @@ import com.example.cellphonesclone.models.User;
 
 public interface IUserService {
     User createUser(UserDTO userDTO) throws DataNotFoundException;
-    String login(String phoneNumber, String password);
+    String login(String phoneNumber, String password) throws Exception;
 }
--- backend/src/main/java/com/example/cellphonesclone/services/UserService.java
+++ backend/src/main/java/com/example/cellphonesclone/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.cellphonesclone.services;
 
 import com.example.cellphonesclone.DTO.UserDTO;
+import com.example.cellphonesclone.components.JwtTokenUtil;
 import com.example.cellphonesclone.exceptions.DataNotFoundException;
 import com.example.cellphonesclone.models.Role;
 import com.example.cellphonesclone.models.User;
@@ -8,14 +9,25 @@ import com.example.cellphonesclone.respositories.RoleRepository;
 import com.example.cellphonesclone.respositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService{
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDTO userDTO;
+    private final AuthenticationManager authenticationManager;
     @Override
+    //register user
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
         String phoneNumber = userDTO.getPhoneNumber();
         if(userRepository.existsByPhoneNumber(phoneNumber)) {
@@ -35,14 +47,31 @@ public class UserService implements IUserService{
         //Kiem tra dang nhap bang google, fb thi kh can password
         if(userDTO.getFacebookAccountID()==0 && userDTO.getGoogleAccountID()==0){
             String password = userDTO.getPassword();
-            // String encodedPassword = passwordEncoder.encode(password);
-            // newUser.setPassword(encodedPassword);
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
         }
         return userRepository.save(newUser);
     }
 
     @Override
-    public String login(String phoneNumber, String password) {
-        return "";
+    public String login(String phoneNumber, String password) throws Exception {
+        Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
+        if (optionalUser.isEmpty()){
+            throw new DataNotFoundException("Invalid phoneNumber/ password");
+        }
+        //return optionalUser.get(); //Muon tra JWT token
+        User existingUser = optionalUser.get();
+        //check password
+        if(userDTO.getFacebookAccountID() == 0 && userDTO.getGoogleAccountID()==0){
+            if(!passwordEncoder.matches(password, existingUser.getPassword())){
+                throw new BadCredentialsException("Wrong phone number or password!");
+            }
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                phoneNumber, password
+        );
+        //authenticate with java spring security
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
 }
