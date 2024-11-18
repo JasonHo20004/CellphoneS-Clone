c6d56bd365fd781b3ccb404f2201dd7bc7f8605c
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-11-18 15:45:40 +0700

    Add JPA configuration and update IUserService interface

--- backend/src/main/java/com/example/cellphonesclone/services/IUserService.java
+++ backend/src/main/java/com/example/cellphonesclone/services/IUserService.java
@@ -1,10 +1,13 @@
 package com.example.cellphonesclone.services;
 
-import com.example.cellphonesclone.DTO.UserDTO;
+import com.example.cellphonesclone.dtos.UpdateUserDTO;
+import com.example.cellphonesclone.dtos.UserDTO;
 import com.example.cellphonesclone.exceptions.DataNotFoundException;
 import com.example.cellphonesclone.models.User;
 
 public interface IUserService {
-    User createUser(UserDTO userDTO) throws DataNotFoundException;
-    String login(String phoneNumber, String password) throws Exception;
+    User createUser(UserDTO userDTO) throws Exception;
+    String login(String phoneNumber, String password, Long roleId) throws Exception;
+    User getUserDetailsFromToken(String token) throws Exception;
+    User updateUser(Long userId, UpdateUserDTO updatedUserDTO) throws Exception;
 }
