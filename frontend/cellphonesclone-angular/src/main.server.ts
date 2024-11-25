fddae871a56aece65ab17e7d04d6bd7307d769e6
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-11-25 13:03:32 +0700

    Add new components for product detail, login, order confirmation, and registration

--- frontend/cellphonesclone-angular/src/main.server.ts
+++ frontend/cellphonesclone-angular/src/main.server.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HomeComponent } from './app/home/home.component';
 import { config } from './app/app.config.server';
+import { OrderComponent } from './app/order/order.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-const bootstrap = () => bootstrapApplication(HomeComponent, config);
+
+const bootstrap = () => bootstrapApplication(RegisterComponent, config);
 
 export default bootstrap;
