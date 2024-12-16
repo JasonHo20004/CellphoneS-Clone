86599e54487fcfbfbeb96438fbc8430e02f44af1
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-17 02:37:59 +0700

    Update `getUserDetail` method and add local storage functionality

--- frontend/cellphonesclone-angular/src/app/services/user.service.ts
+++ frontend/cellphonesclone-angular/src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import { RegisterDTO } from '../dtos/user/register.dto';
 import { LoginDTO } from '../dtos/user/login.dto';
 import { environment } from '../environments/environment';
 import { HttpUtilService } from './http.util.service';
+import { UserResponse } from '../responses/user/user.response';
 
 @Injectable({
   providedIn: 'root'
@@ -31,12 +32,48 @@ export class UserService {
     return this.http.post(this.apiLogin, loginDTO, this.apiConfig);
   }
 
-  getUserDetail(token: string){
-    return this.http.post(this.apiUserDetail, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      })
-    })
+  getUserDetail(token: string): Observable<UserResponse> {
+    return this.http.post<UserResponse>(
+      this.apiUserDetail,
+      {}, // Ensure the body is an empty object if no payload is required
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        })
+      }
+    );
+  }
+  
+  saveUserResponseToLocalStorage(userResponse?: UserResponse) {
+    try {
+      debugger
+      if(userResponse == null || !userResponse) {
+        return;
+      }
+      // Convert the userResponse object to a JSON string
+      const userResponseJSON = JSON.stringify(userResponse);  
+      // Save the JSON string to local storage with a key (e.g., "userResponse")
+      localStorage.setItem('user', userResponseJSON);  
+      console.log('User response saved to local storage.');
+    } catch (error) {
+      console.error('Error saving user response to local storage:', error);
+    }
+  }
+  getUserResponseFromLocalStorage() {
+    try {
+      // Retrieve the JSON string from local storage using the key
+      const userResponseJSON = localStorage.getItem('user'); 
+      if(userResponseJSON == null || userResponseJSON == undefined) {
+        return null;
+      }
+      // Parse the JSON string back to an object
+      const userResponse = JSON.parse(userResponseJSON!);  
+      console.log('User response retrieved from local storage.');
+      return userResponse;
+    } catch (error) {
+      console.error('Error retrieving user response from local storage:', error);
+      return null; // Return null or handle the error as needed
+    }
   }
 }
