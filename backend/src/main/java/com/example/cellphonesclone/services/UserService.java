2c925c736c64f3ba90803a4e612f588f257001a7
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-17 11:16:19 +0700

    Update user information with password encoding

--- backend/src/main/java/com/example/cellphonesclone/services/UserService.java
+++ backend/src/main/java/com/example/cellphonesclone/services/UserService.java
@@ -137,15 +137,18 @@ public class UserService implements IUserService {
     @Transactional
     @Override
     public User updateUser(Long userId, UpdateUserDTO updatedUserDTO) throws Exception {
+        // Find the existing user by userId
         User existingUser = userRepository.findById(userId)
                 .orElseThrow(() -> new DataNotFoundException("User not found"));
 
+        // Check if the phone number is being changed and if it already exists for another user
         String newPhoneNumber = updatedUserDTO.getPhoneNumber();
         if (!existingUser.getPhoneNumber().equals(newPhoneNumber) &&
                 userRepository.existsByPhoneNumber(newPhoneNumber)) {
             throw new DataIntegrityViolationException("Phone number already exists");
         }
 
+        // Update user information based on the DTO
         if (updatedUserDTO.getFullName() != null) {
             existingUser.setFullName(updatedUserDTO.getFullName());
         }
@@ -165,15 +168,18 @@ public class UserService implements IUserService {
             existingUser.setGoogleAccountID(updatedUserDTO.getGoogleAccountId());
         }
 
-        // Simple password update without encoding
+        // Update the password if it is provided in the DTO
         if (updatedUserDTO.getPassword() != null
                 && !updatedUserDTO.getPassword().isEmpty()) {
             if(!updatedUserDTO.getPassword().equals(updatedUserDTO.getRetypePassword())) {
                 throw new DataNotFoundException("Password and retype password not the same");
             }
-            existingUser.setPassword(updatedUserDTO.getPassword());
+            String newPassword = updatedUserDTO.getPassword();
+            String encodedPassword = passwordEncoder.encode(newPassword);
+            existingUser.setPassword(encodedPassword);
         }
-
+        //existingUser.setRole(updatedRole);
+        // Save the updated user
         return userRepository.save(existingUser);
     }
 
