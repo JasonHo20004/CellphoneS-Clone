016cc5ca3d4d0e96798f28aa13d7cb67fc7d4ccb
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-17 10:04:34 +0700

    Update header component for active navigation and user actions

--- frontend/cellphonesclone-angular/src/app/components/header/header.component.ts
+++ frontend/cellphonesclone-angular/src/app/components/header/header.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { UserResponse } from '../../responses/user/user.response';
+import { UserService } from '../../services/user.service';
+import { TokenService } from '../../services/token.service';
+import { CommonModule } from '@angular/common';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, NgbPopoverModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit{
+  userResponse?:UserResponse | null;
+  isPopoverOpen = false;
+  activeNavItem: number = 0;
+
+  constructor(
+    private userService: UserService,       
+    private tokenService: TokenService,    
+    private router: Router,
+  ) {
+    
+   }
+  ngOnInit() {
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();    
+  }  
+
+  togglePopover(event: Event): void {
+    event.preventDefault();
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
+
+  handleItemClick(index: number): void {
+    //alert(`Clicked on "${index}"`);
+    if(index === 0) {
+      debugger
+      this.router.navigate(['/user-profile']);                      
+    } else if (index === 2) {
+      this.userService.removeUserFromLocalStorage();
+      this.tokenService.removeToken();
+      // this.userResponse = this.userService.getUserResponseFromLocalStorage();    
+      this.userResponse = null;
+    }
+    this.isPopoverOpen = false; // Close the popover after clicking an item    
+  }
+
+  
+  setActiveNavItem(index: number) {    
+    this.activeNavItem = index;
+    //alert(this.activeNavItem);
+  }
 }
